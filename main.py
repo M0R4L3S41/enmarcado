928a85d6a3b78704d41f7b05db93c2530581a797
M0R4L3S41 <58787753+M0R4L3S41@users.noreply.github.com>
2024-10-11 08:13:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ ESTADOS = {
 # Función para verificar si el sistema está dentro del horario de funcionamiento
 def is_within_working_hours():
     now = datetime.now()
+    print(f"Hora actual del servidor: {now.strftime('%Y-%m-%d %H:%M:%S')}")  # Imprimir la hora del servidor en consola
     start_time = now.replace(hour=9, minute=0, second=0, microsecond=0)
     end_time = now.replace(hour=17, minute=0, second=0, microsecond=0)
     return start_time <= now <= end_time
